97e3a042b9b6167d94e95edc2f2bf08d86ad043e
Mamta Gupta <mamta.gupta@godigitaltc.com>
2022-05-05 00:33:38 +0530

    Requirement.txt file added

--- MiniArticles/settings.py
+++ MiniArticles/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*pm%lke@(9je*bfm%n7pk18b(nw^2ieq(2u-t+%8d4l^p*89+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mini-articles.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MiniArticles.wsgi --log-file
