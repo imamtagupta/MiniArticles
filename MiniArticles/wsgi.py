0c9d2c5eb2f6433f1391f2d1fa4a400c3275babe
Mamta Gupta <mamta.gupta@godigitaltc.com>
2022-05-08 02:24:46 +0530

    wsgi.py file updated with dj-static

--- MiniArticles/wsgi.py
+++ MiniArticles/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiniArticles.settings')
 
 application = get_wsgi_application()
+
+if not settings.DEBUG:
+    try:
+        from dj_static import Cling
+        application = Cling(get_wsgi_application())
+    except:
+        pass
