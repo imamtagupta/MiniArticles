09b1955e4f93a8e2102a417bc07ac3e036b7d18d
Mamta Gupta <mamta.gupta@godigitaltc.com>
2022-05-08 01:43:58 +0530

    heroku deployment additions

--- MiniArticles/settings.py
+++ MiniArticles/settings.py
@@ -122,9 +122,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
